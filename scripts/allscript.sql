c636ee2c430ca55bddd26a155d73182166797e5a
imenebh2611 <imene.ben.haddada@devoteam.com>
2024-10-03 17:41:39 +0200

    modify script

--- scripts/allscript.sql
+++ scripts/allscript.sql
@@ -1,1 +1,7 @@
 LIST @IBH_REPO/branches/main/snowflake_objects;
+
+DESCRIBE TABLE MY_INVENTORY;
+
+EXECUTE IMMEDIATE FROM @IBH_REPO/branches/main/snowflake_objects/tables/inventory.sql ;
+
+DESCRIBE TABLE MY_INVENTORY;
